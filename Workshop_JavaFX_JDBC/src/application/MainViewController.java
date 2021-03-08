c959c68c0fa16e4c837da797ba136e348dd6e487
Marcio Guilherme Bronzato de Avellar <mgb.avellar@gmail.com>
2021-03-08 10:12:01 -0300

    MainView controller

--- Workshop_JavaFX_JDBC/src/application/MainViewController.java
+++ Workshop_JavaFX_JDBC/src/application/MainViewController.java
@@ -1,4 +1,41 @@
 package application;
 
-public class MainViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    private MenuItem menuItemSeller;
+    @FXML
+    private MenuItem menuItemDepartment;
+    @FXML
+    private MenuItem menuItemAbout;
+
+    // Acima, as correspondências com os botões do Menu de nossa tela principal.
+    // Abaixo, declaramos os métodos para tratar cada uma das ações dos itens do menu
+    //   (implementação temporária no momento)
+
+    @FXML
+    public void onMenuItemSellerAction() {
+        System.out.println("onMenuItemSellerAction");
+    }
+    @FXML
+    public void onMenuItemDepartmentAction() {
+        System.out.println("onMenuItemDepartmentAction");
+    }
+    @FXML
+    public void onMenuItemAboutAction() {
+        System.out.println("onMenuItemAboutAction");
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
