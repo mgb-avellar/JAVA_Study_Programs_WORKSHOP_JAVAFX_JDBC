1fe331031d21b05bf786eb62f4349750dd2b0813
Marcio Guilherme Bronzato de Avellar <mgb.avellar@gmail.com>
2021-03-08 09:44:52 -0300

    MainView design

--- Workshop_JavaFX_JDBC/src/application/Main.java
+++ Workshop_JavaFX_JDBC/src/application/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +12,20 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-        Parent root = loader.load();
-        Scene mainScene = new Scene(root);
+
+        //Parent root = loader.load();
+
+        /* Para ajustar minha cena à janela, faremos modificações aqui, não no Scene Builder
+           A primeira coisa a fazer é comentar (deletar, na verdade, mas deixo isso para o
+           próximo commit) a linha 'Parent root = loader.load();', fazendo novas, como
+           as três próximas abaixo:
+         */
+
+        ScrollPane scrollPane = loader.load();
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+
+        Scene mainScene = new Scene(scrollPane);
         primaryStage.setTitle("Sample JavaFX application");
         primaryStage.setScene(mainScene);
         primaryStage.show();
